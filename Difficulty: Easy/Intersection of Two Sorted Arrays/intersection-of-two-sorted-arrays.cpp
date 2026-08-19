9c5035218f7b2108f262fba5f8863cb236ae34df
Pradnya Jadhav <pradnya.jadhav.it@gmail.com>
2026-08-20 02:15:57 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Intersection of Two Sorted Arrays/intersection-of-two-sorted-arrays.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> intersection(vector<int> &arr1, vector<int> &arr2) {
+
+        vector<int> ans;
+        int i = 0, j = 0;
+
+        while(i < arr1.size() && j < arr2.size()) {
+
+            if(arr1[i] == arr2[j]) {
+
+                if(ans.empty() || ans.back() != arr1[i])
+                    ans.push_back(arr1[i]);
+
+                i++;
+                j++;
+            }
+            else if(arr1[i] < arr2[j]) {
+                i++;
+            }
+            else {
+                j++;
+            }
+        }
+
+        return ans;
+    }
+};
