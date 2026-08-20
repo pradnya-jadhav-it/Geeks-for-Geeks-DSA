d8dca4165f3178ca64ca8503175264353d31ca91
Pradnya Jadhav <pradnya.jadhav.it@gmail.com>
2026-08-20 21:23:14 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Check if two strings are same or not/check-if-two-strings-are-same-or-not.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool areStringsSame(string &s1, string &s2) {
+        if(s1.length() != s2.length())
+            return false;
+
+        for(int i = 0; i < s1.length(); i++) {
+            if(s1[i] != s2[i])
+                return false;
+        }
+
+        return true;
+    }
+};
