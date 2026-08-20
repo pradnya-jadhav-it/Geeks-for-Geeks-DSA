ba35ed6613326e7858fed10bfe7d184b69b0bb64
Pradnya Jadhav <pradnya.jadhav.it@gmail.com>
2026-08-20 21:58:56 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Middle of Three/middle-of-three.cpp
@@ -0,0 +1,12 @@
+class Solution {
+  public:
+    int middle(int a, int b, int c) {
+        if ((a > b && a < c) || (a < b && a > c))
+            return a;
+
+        if ((b > a && b < c) || (b < a && b > c))
+            return b;
+
+        return c;
+    }
+};
