c8134884c91393d68030771dbaf7953912c7218d
Pradnya Jadhav <pradnya.jadhav.it@gmail.com>
2026-08-20 21:40:29 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Median of an Array/median-of-an-array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    double findMedian(vector<int> &arr) {
+        sort(arr.begin(), arr.end());
+
+        int n = arr.size();
+
+        if (n % 2 == 1)
+            return arr[n / 2];
+        else
+            return (arr[n / 2] + arr[n / 2 - 1]) / 2.0;
+    }
+};
